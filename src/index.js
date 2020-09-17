0e531541e954dcbaf2201e83ff59cb3f2ec854c6
Alexander Krasovsky <casha_krasovski@mail.ru>
2020-09-17 07:07:27 +0300

    feat: task execution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array=[]) {
+  if(array.length ==0){
+    return 0;
+  }
+  let min=array[0];
+  for(let i = 1;i<array.length;i++){
+    if(array[i]<min) {
+      min=array[i];
+    }
+  }
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array=[]) {
+  if(array.length ==0){
+    return 0;
+  }
+  let max=array[0];
+  for(let i = 1;i<array.length;i++){
+    if(array[i]>max) {
+      max=array[i];
+    }
+  }
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array=[]) {
+  if(array.length ==0){
+    return 0;
+  }
+  let count=0;
+  for(let i = 0;i<array.length;i++){
+    count+=array[i];
+  }
+  let result =count/array.length;
+  return result;
 }
